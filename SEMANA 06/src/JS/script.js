d1eab2ee1704bc8b56ebd92207ccc4032ac7f1de
danielgodinho772 <danielgodinho772@gmail.com>
2025-03-22 01:20:03 -0300

    Initial commit

---
+++ SEMANA 06/src/JS/script.js
@@ -0,0 +1,21 @@
+/*console.log("Hello World");
+console.log(2+2);
+
+var primeiroTipo;
+let segundoTipo;
+const terceiroTipo = 10; 
+
+let nome = "Daniel";
+let idade = 20;
+
+console.log(nome);
+console.log(idade);
+
+nome = idade;
+
+console.log(nome);*/
+
+function hello(){
+    document.getElementById('texto').textContent = "hello world";
+    document.getElementById('texto').style.color = "red";
+}
---
+++ SEMANA 07/EXC LAMPADA/js/script.js
@@ -0,0 +1,7 @@
+function ligar(){
+    document.getElementById('lampada').src='./src/assets/images/acesa.png';
+}
+
+function desligar(){
+    document.getElementById('lampada').src='./src/assets/images/apagada.png';
+}
